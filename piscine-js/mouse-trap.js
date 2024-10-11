a47a770c5e8f15be7e7984bb953ef2f12af82a80
MohcineDev <mouhcinedev@gmail.com>
2024-10-11 23:31:38 +0100

    add mouse trap

--- piscine-js/mouse-trap.js
+++ piscine-js/mouse-trap.js
@@ -14,6 +14,13 @@ export function createCircle() {
     })
 }
 export function moveCircle() {
+    let inside = fasle
+    const REC = document.querySelector('.box').getBoundingClientRect()
+    const REC_LEFT = REC.left
+    const REC_TOP = REC.top
+    const REC_WIDTH = REC.width
+    const REC_HEIGHT = REC.height
+
     document.body.addEventListener('mousemove', (e) => {
         const lastCircle = document.querySelector('.circle:last-child')
         console.log(lastCircle);
@@ -22,11 +29,11 @@ export function moveCircle() {
         lastCircle.style.top = `${e.clientY}px`
         console.log(e);
 
-        let rec = document.querySelector('.box').getBoundingClientRect()
-        console.log(rec.left, lastCircle.offsetTop);
-        if (lastCircle.offsetLeft > rec.left && lastCircle.offsetLeft < rec.left+rec.width
-            && lastCircle.offsetTop > rec.top && lastCircle.offsetTop < rec.top+rec.height ) {
-            lastCircle.style.backgroundColor = "red"
+        console.log("lastCircle : ", lastCircle);
+        if (lastCircle.offsetLeft > REC_LEFT && lastCircle.offsetLeft + lastCircle.offsetWidth < REC_LEFT + REC_WIDTH
+            && lastCircle.offsetTop > REC_TOP && lastCircle.offsetTop + lastCircle.offsetWidth < REC_TOP + REC_HEIGHT) {
+            lastCircle.style.backgroundColor = "var(--purple)"
+            inside = true
         }
         else {
             lastCircle.style.backgroundColor = "white"
